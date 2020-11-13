bfddb2a73d36ca511026644b5ba0f16d38424875
kubzie <mckubzie@btinternet.com>
2020-11-13 02:18:34 +0000

    challenges page update

--- src/pages/Challenge.js
+++ src/pages/Challenge.js
@@ -1,14 +1,19 @@
 import React, { useState } from 'react'
 import classnames from 'classnames'
 import Layout from '@theme/Layout'
+import Link from '@docusaurus/Link'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import useBaseUrl from '@docusaurus/useBaseUrl'
 import styles from './styles.module.css'
+import HowCard from '../components/HowCard'
 import { Container, Row} from 'react-bootstrap'
 import vidStyles from './video.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function Challenge() {
     const context = useDocusaurusContext()
+    const { siteConfig = {} } = context
+    const [visibleCodeExample, showCodeExample] = useState('READ')
     return (
         <Layout title="Hello">
             <main className="ArchivePage" >
@@ -17,25 +22,25 @@ function Challenge() {
                     {/* HOW */}
                     <section className={styles.forTracks}>
                         <div className="container" color="blue">
-                          
+                            <div className={classnames('row', styles.responsiveCentered)}>
+                                <div className="col col--6 col--offset-3">
+                                    <h2 className="">We're cooking something up...</h2>
+                                </div>
+                            </div>
+
                             <Row>
                                 <Container>
-                                    <div className="App">
+                                    <div className={classnames('row', styles.responsiveCentered)}>
 
-                                        <h2>We're cooking something up...</h2>
-                                        <br></br>
-                                        <div className={vidStyles.videoContainer} 
-                                            >
-                                             <img className="diagram" src="/static/img/pot.gif"  />
+                                        <div className="col col--6 col--offset-3">  
+                                            <img className="diagram" src="/static/img/pot.gif" />
+                                            <br></br>
+                                            <br></br>
+                                            <h2 className="">Please check back soon!!!</h2>
                                         </div>
-                                        <h2>Please check again soon</h2>
-                                        <br></br>
-                                        <br></br>
-                                        
                                     </div>
                                 </Container>
-                            </Row>
-                            
+                            </Row>   
                         </div>
                     </section>
                 </header>
